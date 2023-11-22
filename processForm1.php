c12c6ab3b357850f68a97845c575ded9efbcad1c
B00144945 <B00144945@mytudublin.ie>
2023-11-22 10:04:32 +0000

    Process Form

---
+++ processForm1.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+    $_SESSION['passengerFN'] = isset($_POST['firstname']) ? $_POST['firstname'] : '';
+    $_SESSION['passengerSN'] = isset($_POST['surname']) ? $_POST['surname'] : '';
+    
+   
+    if (isset($_POST['luggage'])) {
+		
+        $_SESSION['luggage'] = 1;
+        
+        
+        header('Location: luggage.html');
+        exit();
+    } else {
+        
+        $_SESSION['luggage'] = 0;
+
+        
+        header('Location: finalStep.php');
+        exit();
+    }
+}
+?>
